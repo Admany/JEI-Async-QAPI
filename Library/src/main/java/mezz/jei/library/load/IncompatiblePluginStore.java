725a58a774bce1237b88b3373f4dc6cfde1a46a0
Mysticpasta1 <ethana5tc@outlook.com>
2026-04-19 12:43:01 -0500

    remove old readme (will do updated version later)

--- Library/src/main/java/mezz/jei/library/load/IncompatiblePluginStore.java
+++ Library/src/main/java/mezz/jei/library/load/IncompatiblePluginStore.java
@@ -4,7 +4,6 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import mezz.jei.api.IModPlugin;
-import net.minecraft.resources.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
